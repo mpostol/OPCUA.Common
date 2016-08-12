62f93254b1f0b187c3e9dad3e9bca8ff31a8f98d
Mariusz <mpostol@cas.eu>
2016-08-12 09:00:54 +0000

    CommServer.UA.ModelDesigner - OPC Viewer has been removed - Working on CAS.CommServer.UA.Common  - Removed reference to OPC.DA.SDK.Core  - Released 3.00.02-Alpha

--- UA.Common/Properties/AssemblyInfo.cs
+++ UA.Common/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9f3155f3-599c-4a1c-b3fb-16e8cc305051")]
-[assembly: AssemblyVersion("3.00.00.*")]
-[assembly: AssemblyFileVersion("3.00.00")]
+[assembly: AssemblyVersion("3.00.02.*")]
+[assembly: AssemblyFileVersion("3.00.02")]
