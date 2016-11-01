ea86169e7c66717d9fdacc73fc0ad721fbba4118
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-11-01 11:29:26 +0000

    CommServer.UA - new release - added project capturing tag creation script. Issue #: 4881

---
+++ VS/trunk/CommServer.UA/UA.Scripts/CreateTag.cmd
@@ -0,0 +1,13 @@
+
+rem//  $LastChangedDate:  $
+rem//  $Rev:  $
+rem//  $LastChangedBy:  $
+rem//  $URL:  $
+rem//  $Id:  $
+
+
+svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.rel_3_00_03  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.rel_3_00_03"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer.UA/ svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.rel_3_00_03 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.UA.rel_3_00_03"
+
+pause ....
+
