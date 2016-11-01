deed1e3c992bca240676ce73faea40cdfab6bc16
Mariusz <mpostol@cas.eu>
2016-11-01 11:29:26 +0000

    CommServer.UA - new release - added project capturing tag creation script. Issue #: 4881

---
+++ UA.Scripts/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿//_______________________________________________________________
+//  Title   :  Assembly info for: CAS.CommServer.UA.Scripts
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate:  $
+//  $Rev:  $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.CommServer.UA.Scripts")]
+[assembly: AssemblyDescription("Scripts Prototyping Workspace for CAS.CommServer.UA.Scripts")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS CommServer Family")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CommServer")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("CBF6D986-922E-4D3D-A7BF-9AB06B0E744E")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("3.00.03.*")]
+[assembly: AssemblyFileVersion("3.00.03")]
+
