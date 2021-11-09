304cf44913073c137a886bfc44b9dce62381f73e
Mariusz Postol <mpostol@cas.eu>
2021-11-09 11:24:01 +0000

    Make it compliant with VS 2019 #12
    - done
    - 4.01.00-Alpha
    - UT :+1:

--- UA.Common/Properties/AssemblyInfo.cs
+++ UA.Common/Properties/AssemblyInfo.cs
@@ -1,21 +1,12 @@
 ﻿
-//<summary>
-//  Title   : Assembly info for: CAS.UACommon
-//  System  : Microsoft Visual C# .NET
-//  $LastChangedDate: 2013-04-17 11:14:21 +0200 (Śr, 17 kwi 2013) $
-//  $Rev: 9137 $
-//  $LastChangedBy: mpostol $
-//  $URL: svn://svnserver.hq.cas.com.pl/VS/trunk/PR45-DataProviderDiagnostic/Properties/AssemblyInfo.tt $
-//  $Id: AssemblyInfo.tt 9137 2013-04-17 09:14:21Z mpostol $
+//__________________________________________________________________________________________________
 //
-//  Copyright (C) 2019, Mariusz Postol LODZ POLAND.
-//  +48 (608) 619 899
-//  mpostol@cas.eu
-//  www.cas.eu
-//</summary>
+//  Copyright (C) 2021, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GitHub: https://github.com/mpostol/OPC-UA-OOI/discussions
+//__________________________________________________________________________________________________
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("CAS.UACommon")]
@@ -23,10 +14,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mariusz Postol")]
 [assembly: AssemblyProduct("Object Oriented Internet")]
-[assembly: AssemblyCopyright("Copyright (C) 2019, Mariusz Postol LODZ POLAND.")]
+[assembly: AssemblyCopyright("Copyright (C) 2021, Mariusz Postol LODZ POLAND.")]
 [assembly: AssemblyTrademark("CommServer")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9f3155f3-599c-4a1c-b3fb-16e8cc305051")]
-[assembly: AssemblyVersion("3.00.05.*")]
-[assembly: AssemblyFileVersion("3.00.05")]
+[assembly: AssemblyVersion("4.01.00.*")]
+[assembly: AssemblyFileVersion("4.01.00")]
